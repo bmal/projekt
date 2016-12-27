6dd616b5567a71331f319fb998c0a1d0798b3be9
Bartek <bartekm93@gmail.com>
2016-12-27 21:38:23 +0100

    willDie? added

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     end
 
     def update
-        if params[:parameter_1] != "" and params[:parameter_2] != ""
+        if params[:parameter_1] != "" and params[:parameter_2] != "" and willDie?
             redirect_to "/good"
         else
             redirect_to "/bad"
@@ -15,19 +15,18 @@ class WelcomeController < ApplicationController
     end
 
     def bad
-        n = 10
-        beta_0 = 1
-        beta_1 = 0.25
-        alpha = 0.05
-        seed = 23423
-        R.x = (1..n).entries
+    end
 
+    def willDie?
         R.eval <<-EOF
-        set.seed(#{seed})
-        y <- #{beta_0} + #{beta_1}*x + rnorm(#{n})
-        fit <- lm( y ~ x )
-        est <- round(coef(fit),3)
-        pvalue <- summary(fit)$coefficients[2,4]
+        library('nnet')
+        set.seed(14)
+        pwd = getwd()
+        t <- data.frame(#{params[:parameter_1]}, #{params[:parameter_2]},  1, 1, 1, 2, 2, 2, 1, 1, 2) ##{params[:parameter_3]}, #{params[:parameter_4]}, #{params[:parameter_5]}, #{params[:parameter_6]}, #{params[:parameter_7]}, #{params[:parameter_8]}, #{params[:parameter_9]}, #{params[:parameter_10]})
+        names(t) <- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10")
+        pred <- predict(get(load("classifier.RData")), t, type = "class")
         EOF
+
+        R.pred.to_i == 2 ? true : false
     end
 end

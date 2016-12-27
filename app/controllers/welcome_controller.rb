c046c274f87cd737ea7a01f3f7027ce380160daa
Pawel Blicharz <pawel.blicharz@nokia.com>
2016-12-27 11:30:55 +0100

    added form for 2 parameters, added handling post method in controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+    def index
+    end
+
+    def update
+        puts "Received parameters:"
+        puts params[:parameter_1]
+        puts params[:parameter_2]
+        redirect_to :root
+    end
 end

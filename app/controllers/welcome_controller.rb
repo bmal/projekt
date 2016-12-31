2aef2bf4258cea3c1f2b066534857ff273f8e801
Lastramus <bananowiec2222@gmail.com>
2016-12-31 13:42:27 +0100

    added fully fledged views for good and bad result

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,7 +22,7 @@ class WelcomeController < ApplicationController
         library('nnet')
         set.seed(14)
         pwd = getwd()
-        t <- data.frame(#{params[:parameter_1]}, #{params[:parameter_2]},  1, 1, 1, 2, 2, 2, 1, 1, 2) ##{params[:parameter_3]}, #{params[:parameter_4]}, #{params[:parameter_5]}, #{params[:parameter_6]}, #{params[:parameter_7]}, #{params[:parameter_8]}, #{params[:parameter_9]}, #{params[:parameter_10]})
+        t <- data.frame(#{params[:parameter_1]}, #{params[:parameter_2]}, #{params[:parameter_3]}, #{params[:parameter_4]}, #{params[:parameter_5]}, #{params[:parameter_6]}, #{params[:parameter_7]}, #{params[:parameter_8]}, #{params[:parameter_9]}, #{params[:parameter_10]})
         names(t) <- c("X1", "X2", "X3", "X4", "X5", "X6", "X7", "X8", "X9", "X10")
         pred <- predict(get(load("classifier.RData")), t, type = "class")
         EOF
